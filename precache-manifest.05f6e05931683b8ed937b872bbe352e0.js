b2329947c4782da9953125f7e95b0f43869984cb
hoainam0503 <14520549@gm.uit.edu.vn>
2019-10-12 12:58:33 +0700

    init

---
+++ precache-manifest.05f6e05931683b8ed937b872bbe352e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cba48f9591e0a2dc5a34b69d6b6a2ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c3dd742a13886c465f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f73a9ae7b7c46c148666",
+    "url": "/static/js/2.f4fb0eaa.chunk.js"
+  },
+  {
+    "revision": "34c3dd742a13886c465f",
+    "url": "/static/js/main.4dece98e.chunk.js"
+  },
+  {
+    "revision": "e200043cf3ba26caef5e",
+    "url": "/static/js/runtime~main.7b24f9fb.js"
+  }
+]);
